2c4c4895134827295f241e8e364e0834f31142cb
root <root@ro.ro>
2019-12-26 14:32:03 -0500

    Updates

---
+++ precache-manifest.1e032faaa5ca4712abbf55a417f10cf8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fe28a422e107aee9e754b5abc7a14f9",
+    "url": "/Image-Classification-ML-App/index.html"
+  },
+  {
+    "revision": "f4a9101aff1c9be928b1",
+    "url": "/Image-Classification-ML-App/static/css/main.9083b7eb.chunk.css"
+  },
+  {
+    "revision": "58a52ecc95f89ee0e0f2",
+    "url": "/Image-Classification-ML-App/static/js/2.f4b0782c.chunk.js"
+  },
+  {
+    "revision": "4cf3fb21e0f63248cb7862bf48b1b37d",
+    "url": "/Image-Classification-ML-App/static/js/2.f4b0782c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f4a9101aff1c9be928b1",
+    "url": "/Image-Classification-ML-App/static/js/main.34d6f462.chunk.js"
+  },
+  {
+    "revision": "7a5dcc5c7a7e8f948e59",
+    "url": "/Image-Classification-ML-App/static/js/runtime-main.3e9892d8.js"
+  },
+  {
+    "revision": "2d0815693912174c27a3c7721579efc5",
+    "url": "/Image-Classification-ML-App/static/media/logo.2d081569.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Image-Classification-ML-App/precache-manifest.1e032faaa5ca4712abbf55a417f10cf8.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Image-Classification-ML-App/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
