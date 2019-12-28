2a11f0c1c4d168e37fea3fd4f199018b997d86bb
Ro <50117686+progamandoconro@users.noreply.github.com>
2019-12-28 23:45:42 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,71 +1,110 @@
 import React, { useState } from 'react';
 import './App.css';
-import logo from './assets/logo.png';
-
+import photo from './assets/cat.jpg';
+import { loadImage } from 'canvas';
 import * as mobilenet from '@tensorflow-models/mobilenet';
+import robot from './assets/robot.jpg';
 
 import './firebase';
 import { storage } from 'firebase';
 
 const App = () => {
-	const [ image, setImage ] = useState([]);
-	const [ url, setURL ] = useState('');
-	const [ predictions, handlePredictions ] = useState([]);
+	const [example, handleExample] = useState([]);
+	const [image, setImage] = useState([]);
+	const [url, setURL] = useState('');
+	const [predictions, handlePredictions] = useState([]);
+	const [spinner, handleSpinner] = useState(true);
 
-	const handleImage = (e) => {
-		setImage(e.target.files[0]);
-		console.log(image);
+	const myPhoto = () => {
+		return <img width="500" height="300" style={{ padding: '10px' }} id="img" src={photo} alt="foto "></img>;
+	};
+	const myExamplePrediction = async () => {
+		const loadModel = await mobilenet.load();
+		const pic = await loadImage(photo);
+		const pred = await loadModel.classify(pic);
+
+		console.log(pred);
+		handleExample(pred);
+	};
+
+	const showSpinner = () => {
+		if (url === '' && spinner === true) {
+			setURL(robot);
+			handleSpinner(false);
+		} else {
+			handleSpinner(true);
+		}
+	};
+
+	const handleImage = async (e) => {
+		setImage(e);
+
+		await db.put(e);
+		db.getDownloadURL().then((e) => setURL(e));
 	};
 	const db = storage().ref('/UserPics/' + Math.random());
 
-	const uploadImage = async () => {
-		await db.put(image);
-		db.getDownloadURL().then((e) => setURL(e));
-	};
-
-	const myPrediction = async () => {
+	const myRealPrediction = async () => {
 		const loadModel = await mobilenet.load();
 		const img = document.getElementById('image');
 		img.setAttribute('crossOrigin', '');
 		console.log(img);
 		await loadModel.classify(img).then(function(p) {
 			// Classify the image
-			console.log('Predictions: ', p);
+			console.log('Predictions: ', image, p);
 			handlePredictions(p);
 		});
+		handleSpinner(true);
 	};
 
 	return (
 		<div className="App">
-			<h1>Real Time Machine Learning for Image Classification</h1>
+			<h1>Machine Learning real time image classification </h1>
 
-			<img src={logo} alt="img" />
-
+			<h5>Example: </h5>
+			{myPhoto()}
+			<button className="button" onClick={(e) => myExamplePrediction(e)}>
+				Predict
+			</button>
+			{example.map((e, k) => (
+				<span key={k}>
+					<h5>{e.className + ': ' + Math.round(e.probability * 100) + '%'}</h5>
+				</span>
+			))}
 			<br />
-			<hr />
-			<h1>Instructions:</h1>
-			<h5>Add target image</h5>
-			<input type="file" onChange={(e) => handleImage(e)} />
+			<h2>Browse and Upload an image: </h2>
 
-			<br />
-			<h5>Upload the image</h5>
-			<button
+			<input
+				className="button"
+				type="file"
+				onChange={(e) => handleImage(e.target.files[0])}
 				onClick={(e) => {
-					uploadImage(e.target.files);
+					showSpinner();
+					handleImage(e.target.files[0]);
+				}}
+			/>
+
+			<br />
+
+			<hr />
+
+			<button
+				className="button"
+				onClick={() => {
+					myRealPrediction();
+					showSpinner();
 				}}
 			>
-				Upload
+				Classification:
 			</button>
-			<img id="image" src={url || 'https://via.placeholder.com/400x300'} alt="" crossOrigin="anonymous" />
-			<br />
-			<h5>Click predict and wait classification to appear</h5>
-			<button onClick={() => myPrediction()}> Predict</button>
-			<hr />
+			<img id="image" src={url} alt="" height="300" width="500" crossOrigin="anonymous" />
+			<h1>AI Classification:</h1>
 			{predictions.map((e, k) => (
-				<li key={k}>
-					<h1>{e.className + ': ' + Math.round(e.probability * 100) + '%'}</h1>
-				</li>
+				<span key={k}>
+					<h5>{e.className + ': ' + Math.round(e.probability * 100) + '%'}</h5>
+				</span>
 			))}
+			<br />
 		</div>
 	);
 };
