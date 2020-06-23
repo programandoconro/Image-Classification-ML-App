2ec993b62459bc2e27c4fe2cfe8b7233c286576a
python <programandoconro@gmail.com>
2020-06-23 19:24:01 +0100

    server deploy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./App.css";
-import photo from "./assets/cat.jpg";
-import { loadImage } from "canvas";
 import * as mobilenet from "@tensorflow-models/mobilenet";
 import robot from "./assets/robot.jpg";
 
@@ -10,33 +8,10 @@ import { storage } from "firebase";
 import firebase from "firebase/app";
 
 const App = () => {
-  const [example, handleExample] = useState([]);
   const [image, setImage] = useState([]);
   const [url, setURL] = useState("");
   const [predictions, handlePredictions] = useState([]);
   const [spinner, handleSpinner] = useState(true);
-
-  const myPhoto = () => {
-    return (
-      <img
-        width="500"
-        height="300"
-        style={{ padding: "10px" }}
-        id="img"
-        src={photo}
-        alt="foto "
-      ></img>
-    );
-  };
-
-  const myExamplePrediction = async () => {
-    const loadModel = await mobilenet.load();
-    const pic = await loadImage(photo);
-    const pred = await loadModel.classify(pic);
-
-    console.log(pred);
-    handleExample(pred);
-  };
 
   const showSpinner = () => {
     if (url === "" && spinner === true) {
@@ -80,16 +55,6 @@ const App = () => {
     <div className="App">
       <h1>Machine Learning real time image classification </h1>
 
-      <h5>Example: </h5>
-      {myPhoto()}
-      <button className="button" onClick={e => myExamplePrediction(e)}>
-        Predict
-      </button>
-      {example.map((e, k) => (
-        <span key={k}>
-          <h5>{e.className + ": " + Math.round(e.probability * 100) + "%"}</h5>
-        </span>
-      ))}
       <br />
       <h2>Browse and Upload an image: </h2>
 
@@ -116,11 +81,12 @@ const App = () => {
       >
         Classification:
       </button>
+    <br />
       <img
         id="image"
         src={url}
         alt=""
-        height="300"
+        height="500"
         width="500"
         crossOrigin="anonymous"
       />
--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase';
 //agrega tus credenciales
 const config = {
-	apiKey: 'xxxxxxxxxxxxxxxxxxxxxx',
-	authDomain: 'xxxxxxxxxxxxxxxxxxxx',
-	databaseURL: 'xxxxxxxxxxxxxxxxxxxxxxx',
-	projectId: 'xxxxxxxxxxxxxxxxxxxxxxxx',
-	storageBucket: 'xxxxxxxxxxxxxxxxxxx',
-	messagingSenderId: 'xxxxxxxxxxxxxxxxxxxx',
-	appId: 'xxxxxxxxxxxxxxxxxxxxxxxxxx',
-	measurementId: 'xxxxxxxxxxxxxxx'
+    apiKey: "AIzaSyBIUa0UFimfB4uxT4kOGW_2ezcW6fGfw-8",
+    authDomain: "ml-image-recognition-67ee0.firebaseapp.com",
+    databaseURL: "https://ml-image-recognition-67ee0.firebaseio.com",
+    projectId: "ml-image-recognition-67ee0",
+    storageBucket: "ml-image-recognition-67ee0.appspot.com",
+    messagingSenderId: "1030876500796",
+    appId: "1:1030876500796:web:158f05589ab52a047e8439",
+    measurementId: "G-L49GGZKYLL"
 };
 
 if (!firebase.apps.length) {
